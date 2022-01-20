fe978bbfcc110b922671aac750c15a595e6ebec0
Élise <elise@eiss.fr>
2022-01-20 12:09:18 +0100

    added express and an example route

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 let hello: string = "hello world";
 
 console.log(hello);
+
+import { Webserver } from "./webserver";
+
+let webserv = new Webserver();
+
+webserv.start(3000);
---
+++ src/webserver/webserver.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import * as http from "http";
+
+export class Webserver {
+  app: express.Application;
+  server?: http.Server;
+  constructor() {
+    this.app = express();
+
+    this.app.get("/", (req, res) => {
+      res.send("hello!");
+    });
+  }
+
+  start(port: number) {
+    this.server = this.app?.listen(port);
+    // return new Promise<void>((resolve, reject) => {
+    //   this.server = this.app.listen(port, () => {
+    //     // if (this.server.)
+    // resolve();
+    //   });
+    //   this.server.on("listening", () => resolve());
+    // });
+  }
+}
