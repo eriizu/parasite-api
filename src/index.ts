359bdafb8e919c72a6b69f8a1531aad09f002721
Élise <elise@eiss.fr>
2022-01-23 23:25:20 +0100

    integraded with typeorm and postgres

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,31 @@
-let hello: string = "hello world";
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { User } from "./entity/User";
 
-console.log(hello);
+createConnection()
+  .then(async (connection) => {
+    console.log("Inserting a new user into the database...");
+    const user = new User();
+    user.firstName = "Timber";
+    user.lastName = "Saw";
+    user.age = 25;
+    await connection.manager.save(user);
+    console.log("Saved a new user with id: " + user.id);
+
+    console.log("Loading users from the database...");
+    const users = await connection.manager.find(User);
+    console.log("Loaded users: ", users);
+
+    // console.log("Here you can setup and run express/koa/any other framework.");
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
 
 import { Webserver } from "./webserver";
-
 let webserv = new Webserver();
-
+let hello: string = "hello world";
 webserv.start(3000);
-
-import { create_test_db } from "./database";
-
-create_test_db();
+console.log(hello);
